011f269fb10792bc88d516903c54f10569752c51
Guldanx <nicolas.gleize@gmail.com>
2015-07-15 11:56:42 +1000

    using sorted from the start, restricting the number of result from the go to improve the speed, and adding the TODOs

--- my_autocomplete.py
+++ my_autocomplete.py
@@ -1,12 +1,17 @@
 #my_autocomplete.py
 import sys
 
-MYDB = ['Mass Effect', 'Mass Effect 2', 'Mass Effect 3', 'Mass Effect: Andromeda',
+# TODO: Speed when using more words
+# TODO: Impletement fuzziness -> 'cmputer' should find 'computer'
+# TODO: Space of the data structure handled
+# TODO: How to incorporate it in a web app ?
+
+MYDB = sorted(['Mass Effect', 'Mass Effect 2', 'Mass Effect 3', 'Mass Effect: Andromeda',
         'Sydney', 'Mayenne', 'Paris', 'Nantes', 'Melbourne', 'Barcelona', 'Tokyo', 'Beijin', 'Shanghai', 'Kyoto', 'London',
         'France', 'Australia', 'Japan', 'England', 'China', 'Spain',
         'Ant', 'Tiger', 'Wolf', 'Cat', 'Dog', 'Bird',
         'Car', 'Computer', 'Flat', 'House',
-        'Google', 'Microsoft', 'Atlasian', 'Hipages', 'Smile']
+        'Google', 'Microsoft', 'Atlasian', 'Hipages', 'Smile'])
 
 def main():
     if len(sys.argv) == 1:
@@ -22,8 +27,8 @@ def show_helper():
 
 def my_autocomplete(query):
     matching_keywords = get_matching_keywords(query)
-    if len(matching_keywords) > 10:
-        matching_keywords = matching_keywords[0:10] + ['...']
+    if len(matching_keywords) == 11:
+        matching_keywords[10] = '...'
     if matching_keywords:
         print('\n'.join(matching_keywords))
     return True
@@ -32,12 +37,16 @@ def get_matching_keywords(query):
     res = []
     second_res = []
     for keyword in MYDB:
+        if len(res) == 11:
+            break
         if keyword.lower().startswith(query.lower()):
             res.append(keyword)
     for keyword in MYDB:
+        if len(res) + len(second_res) == 11:
+            break
         if query.lower() in keyword.lower() and keyword not in res:
             second_res.append(keyword)
-    return sorted(res) + sorted(second_res)
+    return res + second_res
 
 if __name__ == '__main__':
     main()
